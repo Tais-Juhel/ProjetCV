c9b818930728893ced2fbb1acda81b64cfce116d
tjuhel <juhel.tais@ynov.com>
2019-05-16 08:07:43 +0200

    accueil

--- index.php
+++ index.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="/css/indx.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Rajdhani" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>TAÏS JUHEL</h1>
+    <div>
+        <h3 class="left">CURIEUX</h3>
+        <h3 class="right">SOCIABLE</h3>
+        <img src="/img/photoProfil.jpg" alt="photoProfil"/>
+        <h3 class="left down">DYNAMIQUE</h3>
+        <h3 class="right down">IMAGINATIF</h3>
+        <h3>APPLIQUE</h3>
+    </div>
+    <div>
+        <a href="portfolio.php"><input type="button" name="Portfolio" value="Portfolio"></a>
+        <a><input type="button" name="CV" value="Télécharger mon CV"></a>
+    </div>
 </body>
 </html>
---
+++ portfolio.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="/css/portfolio.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Rajdhani" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <a id="logoNav" href="#"><img src="img/monLogo.png" alt="monLogo"/></a>
+            <li><a href="#">Compétences</a></li>
+            <li><a href="#">Projets</a></li>
+            <li><a href="#">Dîplomes</a></li>
+            <li><a href="#">Expérience</a></li>
+            <li><a href="#">Loisirs</a></li>
+            <a id="contactNav" href="#">Contact</a>
+        </ul>
+    </nav>
+
+    <div id="profil">
+        <div>
+            <img src="img/photoProfil2.jpg" alt="photoProfil2"/>
+        </div>
+    </div>
+</body>
+</html>
