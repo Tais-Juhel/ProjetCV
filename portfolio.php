286b61468491541ef30f6efe228693e9b5c8eecc
tjuhel <juhel.tais@ynov.com>
2019-05-29 19:03:11 +0200

    projetArticle

--- portfolio.php
+++ portfolio.php
@@ -6,10 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="/css/portfolio.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Rajdhani" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:700&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
-    <nav>
+    <nav id="menu">
         <ul>
             <a id="logoNav" href="#"><img src="img/monLogo.png" alt="monLogo"/></a>
             <li><a href="#">Compétences</a></li>
@@ -21,14 +22,71 @@
         </ul>
     </nav>
 
-    <div id="profil">
+    <section id="profil">
         <div>
             <img src="img/photoProfil2.jpg" alt="photoProfil2"/>
 
             <table>
-                
+                <tbody>
+                    <tr>
+                        <th>Nom</th>
+                        <td>Taïs Juhel</td>
+                        <th>Ville</th>
+                        <td>Nantes</td>
+                    </tr>
+                    <tr>
+                        <th>Age</th>
+                        <td>19 ans</td>
+                        <th>Activité</th>
+                        <td>Etudiant en informatique</td>
+                    </tr>
+                </tbody>
             </table>
+    </section>
+
+    <section>
+        <h2>COMPETENCES</h2>
+
+        <nav id="compt">
+            <img src="img/html5.png" alt="html">
+            <img src="img/css3.png" alt="css">
+            <img src="img/javascript.png" alt="js">
+            <img src="img/jquery.png" alt="jquery">
+            <img src="img/php.png" alt="php">
+            <img src="img/python.png" alt="python">
+            <img src="img/postgre.png" alt="pgsql">
+            <img src="img/mysql.png" alt="mysql">
+            <img src="img/java.png" alt="java">
+            <img src="img/xd.png" alt="xd">
+            <img src="img/illustrator.png" alt="illustrator">
+        </nav>
+    </section>
+
+    <section>
+        <h2>PROJETS</h2>
+
+        <div id="projet">
+            <h3>VOIR LES PROJETS</h3>
         </div>
+    </section>
+
+    <section>
+        <h2>COMPETENCES</h2>
+
+        <nav id="compt">
+            <img src="img/html5.png" alt="html">
+            <img src="img/css3.png" alt="css">
+            <img src="img/javascript.png" alt="js">
+            <img src="img/jquery.png" alt="jquery">
+            <img src="img/php.png" alt="php">
+            <img src="img/python.png" alt="python">
+            <img src="img/postgre.png" alt="pgsql">
+            <img src="img/mysql.png" alt="mysql">
+            <img src="img/java.png" alt="java">
+            <img src="img/xd.png" alt="xd">
+            <img src="img/illustrator.png" alt="illustrator">
+        </nav>
+    </section>
     </div>
 </body>
 </html>
